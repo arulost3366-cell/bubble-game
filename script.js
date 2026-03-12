d142c9b2df5b079a38d39312aaf797997cd45914
arulost3366-cell <arulost3366@gmail.com>
2026-03-12 22:29:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,94 +5,108 @@ var nextNumber = 1;
 var area = document.querySelector("#gameArea");
 
 function shuffle(array){
-  for(let i=array.length-1;i>0;i--){
-    let j=Math.floor(Math.random()*(i+1));
-    [array[i],array[j]]=[array[j],array[i]];
-  }
-  return array;
+for(let i=array.length-1;i>0;i--){
+let j=Math.floor(Math.random()*(i+1));
+[array[i],array[j]]=[array[j],array[i]];
+}
+return array;
 }
 
 function generateBoard(){
 
-  area.innerHTML="";
+area.innerHTML="";
 
-  nextNumber=1;
-  document.querySelector("#hitVal").innerHTML=nextNumber;
+nextNumber=1;
+document.querySelector("#hitVal").innerHTML=nextNumber;
 
-  var numbers=[1,2,3,4,5,6];
-  numbers=shuffle(numbers);
+var numbers=[1,2,3,4,5,6];
+numbers=shuffle(numbers);
 
-  var cols=3;
-  var rows=2;
+var cols=3;
+var rows=2;
 
-  var cellWidth=area.clientWidth/cols;
-  var cellHeight=area.clientHeight/rows;
+var cellWidth=area.clientWidth/cols;
+var cellHeight=area.clientHeight/rows;
 
-  for(let i=0;i<6;i++){
+for(let i=0;i<6;i++){
 
-    var bubble=document.createElement("div");
-    bubble.classList.add("bubble");
+var bubble=document.createElement("div");
+bubble.classList.add("bubble");
 
-    bubble.textContent=numbers[i];
+bubble.textContent=numbers[i];
 
-    var col=i%cols;
-    var row=Math.floor(i/cols);
+var col=i%cols;
+var row=Math.floor(i/cols);
 
-    var offsetX=Math.random()*30;
-    var offsetY=Math.random()*30;
+var offsetX=Math.random()*40;
+var offsetY=Math.random()*40;
 
-    bubble.style.left=(col*cellWidth+30+offsetX)+"px";
-    bubble.style.top=(row*cellHeight+30+offsetY)+"px";
+bubble.style.left=(col*cellWidth+40+offsetX)+"px";
+bubble.style.top=(row*cellHeight+40+offsetY)+"px";
 
-    bubble.onclick=function(){
+bubble.onclick=function(){
 
-      var num=Number(this.textContent);
+var num=Number(this.textContent);
 
-      if(num===nextNumber){
+if(num===nextNumber){
 
-        scoreVal++;
-        document.querySelector("#scoreVal").innerHTML=scoreVal;
+scoreVal++;
+document.querySelector("#scoreVal").innerHTML=scoreVal;
 
-        this.remove();
+this.classList.add("pop");
 
-        nextNumber++;
+setTimeout(()=>{
+this.remove();
+},250);
 
-        if(nextNumber>6){
+nextNumber++;
 
-          setTimeout(()=>{
-            generateBoard();
-          },400);
+if(nextNumber>6){
 
-        }else{
+setTimeout(()=>{
+generateBoard();
+},500);
 
-          document.querySelector("#hitVal").innerHTML=nextNumber;
+}else{
 
-        }
+document.querySelector("#hitVal").innerHTML=nextNumber;
 
-      }
+}
 
-    }
+}else{
 
-    area.appendChild(bubble)
+/* 点击错误闪红 */
 
-  }
+this.style.background="crimson";
+
+setTimeout(()=>{
+this.style.background="royalblue";
+},200);
+
+}
+
+}
+
+area.appendChild(bubble)
+
+}
 
 }
 
 function runTimer(){
 
-  var timer=setInterval(function(){
+var timer=setInterval(function(){
 
-    if(timerVal>0){
-      timerVal--;
-    }else{
-      clearInterval(timer);
-      area.innerHTML="<h1 style='text-align:center;margin-top:200px'>Game Over</h1>";
-    }
+if(timerVal>0){
+timerVal--;
+}else{
+clearInterval(timer);
+area.innerHTML="<h1 style='text-align:center;margin-top:200px'>Game Over</h1>";
+}
 
-    document.querySelector("#timerVal").innerHTML=timerVal;
+document.querySelector("#timerVal").innerHTML=timerVal;
 
-  },1000)
+},1000)
 
 }
 
