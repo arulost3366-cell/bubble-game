3041183d1d7fcb338746a1cc42dc7efcd6d3cb85
arulost3366-cell <arulost3366@gmail.com>
2026-03-12 22:06:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,116 @@
-var timerVal = 300;
-var scoreVal = 0;
-var nextNumber = 1;
-var totalBubbles = 40;
+var timerVal = 300
+var scoreVal = 0
+var nextNumber = 1
+
+var bubbleCount = 10
+
+var area = document.querySelector("#gameArea")
+
+function randomPosition(){
+
+var x = Math.random()*750
+var y = Math.random()*420
+
+return {x,y}
+
+}
 
 function createBubble(num){
-  return `<div class="bubble">${num}</div>`;
+
+var bubble = document.createElement("div")
+
+bubble.classList.add("bubble")
+
+bubble.textContent = num
+
+var pos = randomPosition()
+
+bubble.style.left = pos.x + "px"
+bubble.style.top = pos.y + "px"
+
+bubble.onclick = function(){
+
+if(num === nextNumber){
+
+scoreVal++
+
+document.querySelector("#scoreVal").innerHTML = scoreVal
+
+nextNumber++
+
+if(nextNumber > 5){
+nextNumber = 1
+}
+
+document.querySelector("#hitVal").innerHTML = nextNumber
+
+bubble.remove()
+
+spawnBubble()
+
+}
+
+}
+
+area.appendChild(bubble)
+
+}
+
+function spawnBubble(){
+
+var num
+
+if(Math.random() < 0.5){
+
+num = nextNumber
+
+}else{
+
+num = Math.floor(Math.random()*5)+1
+
+}
+
+createBubble(num)
+
 }
 
 function generateBoard(){
 
-  var numbers=[];
+for(var i=0;i<bubbleCount;i++){
 
-  for(var i=0;i<totalBubbles-1;i++){
-    numbers.push(Math.floor(Math.random()*5)+1);
-  }
+var num = Math.floor(Math.random()*5)+1
 
-  numbers.push(nextNumber);
+createBubble(num)
 
-  numbers.sort(()=>Math.random()-0.5);
+}
 
-  var clutter="";
-
-  numbers.forEach(n=>{
-    clutter+=createBubble(n);
-  });
-
-  document.querySelector("#pbtm").innerHTML=clutter;
 }
 
 function runTimer(){
 
-  var timer=setInterval(function(){
+var timer = setInterval(function(){
 
-    if(timerVal>0){
+if(timerVal > 0){
 
-      timerVal--;
+timerVal--
 
-    }else{
+}else{
 
-      clearInterval(timer);
+clearInterval(timer)
 
-      document.querySelector("#pbtm").innerHTML=
-      `<h1 style="color:royalblue">Game Over</h1>`;
+area.innerHTML = "<h1 style='text-align:center;margin-top:200px'>Game Over</h1>"
 
-    }
-
-    document.querySelector("#timerVal").innerHTML=timerVal;
-
-  },1000);
 }
 
-document.querySelector("#pbtm").addEventListener("click",function(e){
+document.querySelector("#timerVal").innerHTML = timerVal
 
-  if(!e.target.classList.contains("bubble")) return;
+},1000)
 
-  var clickedNum=Number(e.target.textContent);
+}
 
-  if(clickedNum===nextNumber){
+document.querySelector("#hitVal").innerHTML = nextNumber
 
-    scoreVal++;
+generateBoard()
 
-    document.querySelector("#scoreVal").innerHTML=scoreVal;
-
-    nextNumber++;
-
-    if(nextNumber>5) nextNumber=1;
-
-    document.querySelector("#hitVal").innerHTML=nextNumber;
-
-    generateBoard();
-
-  }
-
-});
-
-document.querySelector("#hitVal").innerHTML=nextNumber;
-
-generateBoard();
-
-runTimer();
-
-runTimer();
+runTimer()
 
